2d4c69cd1da13d8de83e4e23a993be0deadf8053
ArtemShevchenko2023 <129595378+ArtemShevchenko2023@users.noreply.github.com>
2023-11-19 10:44:41 +0400

    Update main.py

--- Cucumber/main.py
+++ Cucumber/main.py
@@ -1,10 +1,10 @@
+import os
 import sqlite3
 import discord
 from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
-pon = ".\pon.db"
 @bot.event
 async def on_ready():
     print(f'Вход выполнен как {bot.user}')
@@ -13,33 +13,25 @@ async def тест(ctx):
     a = ctx.author
     await ctx.send(f'Привет {a}!')
 @bot.command()
-async def запись(ctx, name, age):
-    # Подключение к базе данных
-    conn = sqlite3.connect(pon)
+async def запись(ctx, username: str, age: int):
+    conn = sqlite3.connect("user.db")
     c = conn.cursor()
-
-    # Создание таблицы, если она не существует
     c.execute('''CREATE TABLE IF NOT EXISTS users
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER)''')
-
-    # Вставка данных пользователя в таблицу
-    c.execute("INSERT INTO users (name, age) VALUES (?, ?)", (name, age))
+                 (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, age INTEGER)''')
+    c.execute("INSERT INTO users (username, age) VALUES (?, ?)", (username, age))
     conn.commit()
-
-    # Закрытие соединения с базой данных
     conn.close()
-
-    await ctx.send(f'Пользователь {name} успешно записан в базу данных с возрастом {age}!')
+    await ctx.send(f'Пользователь {username} успешно записан в базу данных с возрастом {age}!')
 @bot.command()
-async def юзер(ctx, name: str):
-    conn = sqlite3.connect(pon)
+async def юзер(ctx, username: str):
+    conn = sqlite3.connect("user.db")
     c = conn.cursor()
-    c.execute("SELECT * FROM users WHERE name=?", (name,))
+    c.execute("SELECT * FROM users WHERE username=?", (username,))
     result = c.fetchone()
     conn.close()
     if result:
-        name, age = result
-        await ctx.send(f'Имя пользователя: {name}, Возраст: {age}')
+        fetched_age, fetched_username, _ = result
+        await ctx.send(f'Имя пользователя: {fetched_username}, Возраст: {fetched_age}')
     else:
-        await ctx.send(f'Пользователь {name} не найден.')
+        await ctx.send(f'Пользователь {username} не найден.')
 bot.run("MTE3NTQxMjA5MjI4MzY1NDI0NQ.GdhmFB.c869ev4_h8QwuITS2_JC0Mf9E5ShL2Echvu1j(ponon)A")
